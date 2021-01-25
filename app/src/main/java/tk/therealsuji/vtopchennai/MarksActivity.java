c85c6b49621c4ee4505ea13bafb7279e5649f648
Sujit Kumar <me@therealsuji.tk>
2021-01-25 15:46:28 +0530

    Added feature of filtering marks by course or title

--- app/src/main/java/tk/therealsuji/vtopchennai/MarksActivity.java
+++ app/src/main/java/tk/therealsuji/vtopchennai/MarksActivity.java
@@ -8,6 +8,9 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
@@ -26,7 +29,10 @@ public class MarksActivity extends AppCompatActivity {
     ScrollView marks;
     ArrayList<TextView> buttons = new ArrayList<>();
     ArrayList<LinearLayout> markViews = new ArrayList<>();
+    LinearLayout markButtons;
+    Context context;
     float pixelDensity;
+    SharedPreferences sharedPreferences;
 
     public void setMarks(View view) {
         marks.scrollTo(0, 0);
@@ -51,16 +57,18 @@ public class MarksActivity extends AppCompatActivity {
         ((HorizontalScrollView) findViewById(R.id.markTitlesContainer)).smoothScrollTo((int) location - halfWidth, 0);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_marks);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    public void filterByCourse(MenuItem menuItem) {
+        if (menuItem != null && sharedPreferences.getBoolean("filterByCourse", true)) {
+            return;
+        }
 
-        final Context context = this;
-        final LinearLayout markButtons = findViewById(R.id.markTitles);
-        pixelDensity = context.getResources().getDisplayMetrics().density;
-        marks = findViewById(R.id.marks);
+        sharedPreferences.edit().remove("filterByCourse").apply();
+
+        markButtons.removeAllViews();
+        marks.removeAllViews();
+
+        buttons.clear();
+        markViews.clear();
 
         new Thread(new Runnable() {
             @Override
@@ -68,7 +76,226 @@ public class MarksActivity extends AppCompatActivity {
                 SQLiteDatabase myDatabase = context.openOrCreateDatabase("vtop", Context.MODE_PRIVATE, null);
 
                 myDatabase.execSQL("CREATE TABLE IF NOT EXISTS marks (id INT(3) PRIMARY KEY, course VARCHAR, type VARCHAR, title VARCHAR, score VARCHAR, status VARCHAR, weightage VARCHAR, average VARCHAR, posted VARCHAR)");
-                Cursor c = myDatabase.rawQuery("SELECT DISTINCT title FROM marks", null);
+                Cursor c = myDatabase.rawQuery("SELECT course FROM marks GROUP BY course", null);
+
+                int courseIndex = c.getColumnIndex("course");
+                c.moveToFirst();
+
+                for (int i = 0; i < c.getCount(); ++i, c.moveToNext()) {
+                    String course = c.getString(courseIndex);
+
+                    /*
+                        Creating a the mark view
+                     */
+                    final LinearLayout markView = new LinearLayout(context);
+                    LinearLayout.LayoutParams markViewParams = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT
+                    );
+                    markView.setLayoutParams(markViewParams);
+                    markView.setPadding(0, (int) (65 * pixelDensity), 0, (int) (15 * pixelDensity));
+                    markView.setOrientation(LinearLayout.VERTICAL);
+
+                    markViews.add(markView);    //Storing the view
+
+                    /*
+                        Creating the course button
+                     */
+                    final TextView markButton = new TextView(context);
+                    LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            (int) (25 * pixelDensity)
+                    );
+                    buttonParams.setMarginStart((int) (5 * pixelDensity));
+                    buttonParams.setMarginEnd((int) (5 * pixelDensity));
+                    buttonParams.setMargins(0, (int) (20 * pixelDensity), 0, (int) (20 * pixelDensity));
+                    markButton.setLayoutParams(buttonParams);
+                    markButton.setPadding((int) (20 * pixelDensity), 0, (int) (20 * pixelDensity), 0);
+                    if (i == 0) {
+                        markButton.setBackground(ContextCompat.getDrawable(context, R.drawable.button_secondary_selected));
+                        findViewById(R.id.noData).setVisibility(View.INVISIBLE);
+                    } else {
+                        markButton.setBackground(ContextCompat.getDrawable(context, R.drawable.button_secondary));
+                    }
+                    markButton.setTag(i);
+                    markButton.setText(course);
+                    markButton.setTextColor(getColor(R.color.colorPrimary));
+                    markButton.setTextSize(12);
+                    markButton.setGravity(Gravity.CENTER_VERTICAL);
+                    markButton.setTypeface(ResourcesCompat.getFont(context, R.font.rubik), Typeface.BOLD);
+                    markButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            setMarks(markButton);
+                        }
+                    });
+
+                    /*
+                        Finally adding the button to the HorizontalScrollView
+                     */
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            markButtons.addView(markButton);
+                        }
+                    });
+
+                    buttons.add(markButton);    //Storing the button
+
+                    Cursor s = myDatabase.rawQuery("SELECT * FROM marks WHERE course = '" + course + "' ORDER BY type DESC", null);
+
+                    int titleIndex = s.getColumnIndex("title");
+                    int typeIndex = s.getColumnIndex("type");
+                    int scoreIndex = s.getColumnIndex("score");
+                    int statusIndex = s.getColumnIndex("status");
+                    int weightageIndex = s.getColumnIndex("weightage");
+                    int averageIndex = s.getColumnIndex("average");
+
+                    int[] indexes = {typeIndex, scoreIndex, weightageIndex, averageIndex, statusIndex};
+                    String[] titles = {getString(R.string.type), getString(R.string.score), getString(R.string.weightage), getString(R.string.average), getString(R.string.status)};
+
+                    s.moveToFirst();
+
+                    for (int j = 0; j < s.getCount(); ++j, s.moveToNext()) {
+                        /*
+                            The outer block for the mark
+                         */
+                        final LinearLayout block = new LinearLayout(context);
+                        LinearLayout.LayoutParams blockParams = new LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT
+                        );
+                        blockParams.setMarginStart((int) (20 * pixelDensity));
+                        blockParams.setMarginEnd((int) (20 * pixelDensity));
+                        blockParams.setMargins(0, (int) (5 * pixelDensity), 0, (int) (5 * pixelDensity));
+                        block.setPadding(0, 0, 0, (int) (17 * pixelDensity));
+                        block.setLayoutParams(blockParams);
+                        block.setBackground(ContextCompat.getDrawable(context, R.drawable.plain_card));
+                        block.setOrientation(LinearLayout.VERTICAL);
+
+                        /*
+                            The mark title TextView
+                         */
+                        TextView markTitle = new TextView(context);
+                        TableRow.LayoutParams markTitleParams = new TableRow.LayoutParams(
+                                TableRow.LayoutParams.WRAP_CONTENT,
+                                TableRow.LayoutParams.WRAP_CONTENT
+                        );
+                        markTitleParams.setMarginStart((int) (20 * pixelDensity));
+                        markTitleParams.setMarginEnd((int) (20 * pixelDensity));
+                        markTitleParams.setMargins(0, (int) (20 * pixelDensity), 0, (int) (5 * pixelDensity));
+                        markTitle.setLayoutParams(markTitleParams);
+                        markTitle.setText(s.getString(titleIndex));
+                        markTitle.setTextColor(getColor(R.color.colorPrimary));
+                        markTitle.setTextSize(20);
+                        markTitle.setTypeface(ResourcesCompat.getFont(context, R.font.rubik), Typeface.BOLD);
+
+                        block.addView(markTitle);  //Adding the title to block
+
+                        for (int k = 0; k < 5; ++k) {
+                            String valueString = s.getString(indexes[k]);
+                            if (!valueString.equals("")) {
+                                /*
+                                    The inner block
+                                 */
+                                LinearLayout innerBlock = new LinearLayout(context);
+                                innerBlock.setLayoutParams(new LinearLayout.LayoutParams(
+                                        LinearLayout.LayoutParams.MATCH_PARENT,
+                                        LinearLayout.LayoutParams.WRAP_CONTENT
+                                ));
+                                innerBlock.setOrientation(LinearLayout.HORIZONTAL);
+
+                                /*
+                                    The title TextView
+                                 */
+                                TextView title = new TextView(context);
+                                TableRow.LayoutParams titleParams = new TableRow.LayoutParams(
+                                        TableRow.LayoutParams.WRAP_CONTENT,
+                                        TableRow.LayoutParams.WRAP_CONTENT
+                                );
+                                titleParams.setMarginStart((int) (20 * pixelDensity));
+                                titleParams.setMargins(0, (int) (3 * pixelDensity), 0, (int) (3 * pixelDensity));
+                                title.setLayoutParams(titleParams);
+                                title.setText(titles[k]);
+                                title.setTextColor(getColor(R.color.colorPrimary));
+                                title.setTextSize(16);
+                                title.setTypeface(ResourcesCompat.getFont(context, R.font.rubik));
+
+                                innerBlock.addView(title);  //Adding the title to the inner block
+
+                                /*
+                                    The value TextView
+                                 */
+                                TextView value = new TextView(context);
+                                TableRow.LayoutParams valueParams = new TableRow.LayoutParams(
+                                        TableRow.LayoutParams.MATCH_PARENT,
+                                        TableRow.LayoutParams.WRAP_CONTENT
+                                );
+                                valueParams.setMarginEnd((int) (20 * pixelDensity));
+                                valueParams.setMargins(0, (int) (3 * pixelDensity), 0, (int) (3 * pixelDensity));
+                                value.setLayoutParams(valueParams);
+                                value.setText(valueString);
+                                value.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+                                value.setTextColor(getColor(R.color.colorPrimary));
+                                value.setTextSize(16);
+                                value.setTypeface(ResourcesCompat.getFont(context, R.font.rubik), Typeface.BOLD);
+
+                                innerBlock.addView(value);  //Adding the value to the inner block
+
+                                /*
+                                    Finally adding the inner block to the main block
+                                 */
+                                block.addView(innerBlock);
+                            }
+                        }
+
+                        /*
+                            Finally adding the main block to the view
+                         */
+                        markView.addView(block);
+                    }
+
+                    if (i == 0) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                marks.addView(markView);
+                            }
+                        });
+                    }
+
+                    s.close();
+                }
+
+                c.close();
+                myDatabase.close();
+
+                SharedPreferences sharedPreferences = context.getSharedPreferences("tk.therealsuji.vtopchennai", Context.MODE_PRIVATE);
+                sharedPreferences.edit().putBoolean("newMarks", false).apply();
+            }
+        }).start();
+    }
+
+    public void filterByTitle(MenuItem menuItem) {
+        if (menuItem != null && !sharedPreferences.getBoolean("filterByCourse", true)) {
+            return;
+        }
+
+        sharedPreferences.edit().putBoolean("filterByCourse", false).apply();
+
+        markButtons.removeAllViews();
+        marks.removeAllViews();
+
+        buttons.clear();
+        markViews.clear();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SQLiteDatabase myDatabase = context.openOrCreateDatabase("vtop", Context.MODE_PRIVATE, null);
+
+                myDatabase.execSQL("CREATE TABLE IF NOT EXISTS marks (id INT(3) PRIMARY KEY, course VARCHAR, type VARCHAR, title VARCHAR, score VARCHAR, status VARCHAR, weightage VARCHAR, average VARCHAR, posted VARCHAR)");
+                Cursor c = myDatabase.rawQuery("SELECT title FROM marks GROUP BY title", null);
 
                 int titleIndex = c.getColumnIndex("title");
                 c.moveToFirst();
@@ -134,7 +361,7 @@ public class MarksActivity extends AppCompatActivity {
 
                     buttons.add(markButton);    //Storing the button
 
-                    Cursor s = myDatabase.rawQuery("SELECT * FROM marks WHERE title = '" + markTitle + "'", null);
+                    Cursor s = myDatabase.rawQuery("SELECT * FROM marks WHERE title = '" + markTitle + "' ORDER BY course", null);
 
                     int courseIndex = s.getColumnIndex("course");
                     int typeIndex = s.getColumnIndex("type");
@@ -170,7 +397,7 @@ public class MarksActivity extends AppCompatActivity {
                          */
                         TextView course = new TextView(context);
                         TableRow.LayoutParams courseParams = new TableRow.LayoutParams(
-                                TableRow.LayoutParams.MATCH_PARENT,
+                                TableRow.LayoutParams.WRAP_CONTENT,
                                 TableRow.LayoutParams.WRAP_CONTENT
                         );
                         courseParams.setMarginStart((int) (20 * pixelDensity));
@@ -266,5 +493,32 @@ public class MarksActivity extends AppCompatActivity {
                 sharedPreferences.edit().putBoolean("newMarks", false).apply();
             }
         }).start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marks);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        context = this;
+        markButtons = findViewById(R.id.markTitles);
+        pixelDensity = context.getResources().getDisplayMetrics().density;
+        marks = findViewById(R.id.marks);
+
+        sharedPreferences = this.getSharedPreferences("tk.therealsuji.vtopchennai", MODE_PRIVATE);
+        if (sharedPreferences.getBoolean("filterByCourse", true)) {
+            filterByCourse(null);
+        } else {
+            filterByTitle(null);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.marks_menu, menu);
+
+        return true;
     }
 }
