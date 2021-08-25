2983c5ea4e7df710b32b590a5d117e1ae486ab7d
Sujit Kumar <me@therealsuji.tk>
2021-08-25 14:48:39 +0530

    Added blank academics, courses and profile fragments

--- app/src/main/java/tk/therealsuji/vtopchennai/MainActivity.java
+++ app/src/main/java/tk/therealsuji/vtopchennai/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import tk.therealsuji.vtopchennai.fragments.AcademicsFragment;
+import tk.therealsuji.vtopchennai.fragments.CoursesFragment;
 import tk.therealsuji.vtopchennai.fragments.HomeFragment;
+import tk.therealsuji.vtopchennai.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+    Fragment homeFragment, academicsFragment, coursesFragment, profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,42 @@ public class MainActivity extends AppCompatActivity {
             return windowInsets.consumeSystemWindowInsets();
         });
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_body, new HomeFragment()).commit();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment;
+
+            if (item.getItemId() == R.id.item_academics) {
+                if (academicsFragment == null) {
+                    academicsFragment = new AcademicsFragment();
+                }
+
+                selectedFragment = academicsFragment;
+            } else if (item.getItemId() == R.id.item_courses) {
+                if (coursesFragment == null) {
+                    coursesFragment = new CoursesFragment();
+                }
+
+                selectedFragment = coursesFragment;
+            } else if (item.getItemId() == R.id.item_profile) {
+                if (profileFragment == null) {
+                    profileFragment = new ProfileFragment();
+                }
+
+                selectedFragment = profileFragment;
+            } else {
+                if (homeFragment == null) {
+                    homeFragment = new HomeFragment();
+                }
+
+                selectedFragment = homeFragment;
+            }
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_body, selectedFragment)
+                    .commit();
+
+            return true;
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.item_home);
     }
 }
