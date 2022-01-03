ee001dd2b9af456c5675ab777f30fab175a5ac75
Sujit Kumar <me@therealsuji.tk>
2022-01-03 09:46:20 +0530

    Fix timeout bug in upcoming notifications

--- app/src/main/java/tk/therealsuji/vtopchennai/helpers/NotificationHelper.java
+++ app/src/main/java/tk/therealsuji/vtopchennai/helpers/NotificationHelper.java
@@ -128,6 +128,7 @@ public class NotificationHelper extends ContextWrapper {
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.ic_timetable)
                 .setLargeIcon(SettingsRepository.getBitmapFromVectorDrawable(largeIcon))
+                .setTimeoutAfter(0)     // Resetting the timeout duration
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setWhen(calendar.getTimeInMillis());
     }
