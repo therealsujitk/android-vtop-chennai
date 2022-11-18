3923b188e79dfe08b1414c0df72c5a42d3455a00
Sujit Kumar <me@therealsuji.tk>
2022-11-18 22:22:26 +0530

    Fix material colours in notifications

--- app/src/main/java/tk/therealsuji/vtopchennai/helpers/NotificationHelper.java
+++ app/src/main/java/tk/therealsuji/vtopchennai/helpers/NotificationHelper.java
@@ -9,12 +9,12 @@ import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.util.TypedValue;
 
+import androidx.appcompat.view.ContextThemeWrapper;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
-
-import com.google.android.material.color.MaterialColors;
 
 import java.text.ParseException;
 import java.util.Calendar;
@@ -35,6 +35,7 @@ public class NotificationHelper extends ContextWrapper {
     public static final String CHANNEL_NAME_ONGOING = "Ongoing Classes";
 
     private NotificationManager manager;
+    private final int notificationColor;
 
     public NotificationHelper(Context context) {
         super(context);
@@ -71,7 +72,18 @@ public class NotificationHelper extends ContextWrapper {
             getManager().createNotificationChannel(ongoing);
         }
 
-        this.setTheme(R.style.Base_Theme_VTOP);
+        TypedValue typedValue = new TypedValue();
+        ContextThemeWrapper contextThemeWrapper;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            contextThemeWrapper = new ContextThemeWrapper(this.getApplicationContext(), android.R.style.Theme_DeviceDefault_DayNight);
+            contextThemeWrapper.getTheme().resolveAttribute(android.R.attr.colorAccent, typedValue, true);
+        } else {
+            contextThemeWrapper = new ContextThemeWrapper(this.getApplicationContext(), R.style.Theme_Material3_DayNight);
+            contextThemeWrapper.getTheme().resolveAttribute(android.R.attr.colorPrimary, typedValue, true);
+        }
+
+        this.notificationColor = typedValue.data;
     }
 
     public NotificationManager getManager() {
@@ -83,10 +95,8 @@ public class NotificationHelper extends ContextWrapper {
     }
 
     public NotificationCompat.Builder notifySync(String title, String message) {
-        int colorPrimary = MaterialColors.getColor(this, R.attr.colorPrimary, 0);
-
         return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID_APPLICATION)
-                .setColor(colorPrimary)
+                .setColor(this.getNotificationColor(true))
                 .setContentText(message)
                 .setContentTitle(title)
                 .setSmallIcon(R.drawable.ic_sync)
@@ -113,18 +123,16 @@ public class NotificationHelper extends ContextWrapper {
             largeIcon = ContextCompat.getDrawable(this, R.drawable.ic_theory);
         }
 
-        int colorPrimary = MaterialColors.getColor(this, R.attr.colorPrimary, 0);
-
         assert largeIcon != null;
         largeIcon = DrawableCompat.wrap(largeIcon);
-        DrawableCompat.setTint(largeIcon, colorPrimary);
+        DrawableCompat.setTint(largeIcon, this.getNotificationColor(false));
 
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timetableItem.startTime.split(":")[0]));
         calendar.set(Calendar.MINUTE, Integer.parseInt(timetableItem.startTime.split(":")[1]));
 
         return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID_UPCOMING)
-                .setColor(colorPrimary)
+                .setColor(this.getNotificationColor(true))
                 .setContentIntent(pendingIntent)
                 .setContentText(message)
                 .setContentTitle(title)
@@ -155,11 +163,9 @@ public class NotificationHelper extends ContextWrapper {
             largeIcon = ContextCompat.getDrawable(this, R.drawable.ic_theory);
         }
 
-        int colorPrimary = MaterialColors.getColor(this, R.attr.colorPrimary, 0);
-
         assert largeIcon != null;
         largeIcon = DrawableCompat.wrap(largeIcon);
-        DrawableCompat.setTint(largeIcon, colorPrimary);
+        DrawableCompat.setTint(largeIcon, this.getNotificationColor(false));
 
         Calendar calendarStart = Calendar.getInstance();
         calendarStart.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timetableItem.startTime.split(":")[0]));
@@ -170,7 +176,7 @@ public class NotificationHelper extends ContextWrapper {
         calendarEnd.set(Calendar.MINUTE, Integer.parseInt(timetableItem.endTime.split(":")[1]));
 
         return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID_ONGOING)
-                .setColor(colorPrimary)
+                .setColor(this.getNotificationColor(true))
                 .setContentIntent(pendingIntent)
                 .setContentText(message)
                 .setContentTitle(title)
@@ -179,5 +185,13 @@ public class NotificationHelper extends ContextWrapper {
                 .setTimeoutAfter(calendarEnd.getTimeInMillis() - calendarStart.getTimeInMillis())
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setWhen(calendarStart.getTimeInMillis());
+    }
+
+    private int getNotificationColor(boolean checkBuild) {
+        if (checkBuild && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return NotificationCompat.COLOR_DEFAULT;
+        }
+
+        return this.notificationColor;
     }
 }
